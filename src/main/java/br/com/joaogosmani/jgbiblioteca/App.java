5446d81d815711293f7d09a046704568e6e3d693
Joao Gosmani <joaogosmani@gmail.com>
2023-02-08 16:11:01 -0300

    Projeto criado

---
+++ src/main/java/br/com/joaogosmani/jgbiblioteca/App.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.jgbiblioteca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
