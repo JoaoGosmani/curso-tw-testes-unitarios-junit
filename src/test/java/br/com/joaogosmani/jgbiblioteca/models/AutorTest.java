63e9b1b07b1e1879709807595eeb7af42c945b54
Joao Gosmani <joaogosmani@gmail.com>
2023-02-08 21:19:43 -0300

    Refatorando os testes

--- src/test/java/br/com/joaogosmani/jgbiblioteca/models/AutorTest.java
+++ src/test/java/br/com/joaogosmani/jgbiblioteca/models/AutorTest.java
@@ -1,17 +1,30 @@
 package br.com.joaogosmani.jgbiblioteca.models;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 public class AutorTest {
     
     @Test
-    public void testaMetodoEstaVivo() {
+    void quandoMetodoEstaVivoForChamadoComDataFalecimentoNulaDeveRetornarTrue() {
         var autor = new Autor();
 
         var estaVivo = autor.estaVivo();
 
-        Assertions.assertEquals(true, estaVivo);
+        assertEquals(true, estaVivo);
+    }
+
+    @Test
+    void quandoMetodoEstaVivoForChamadoComDataFalecimentoPreenchidaDeveRetornarFalse() {
+        var autor = new Autor();
+        autor.setDataFalecimento(LocalDate.now());
+
+        var estaVivo = autor.estaVivo();
+
+        assertEquals(false, estaVivo);
     }
 
 }
