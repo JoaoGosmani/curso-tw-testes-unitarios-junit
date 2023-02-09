a0c2b34e0ac82bbe260102a760ef22e3536b5033
Joao Gosmani <joaogosmani@gmail.com>
2023-02-09 15:59:13 -0300

    Refatorando o método de criação de emprestimos

--- src/main/java/br/com/joaogosmani/jgbiblioteca/services/EmprestimoService.java
+++ src/main/java/br/com/joaogosmani/jgbiblioteca/services/EmprestimoService.java
@@ -8,14 +8,19 @@ import br.com.joaogosmani.jgbiblioteca.models.Emprestimo;
 import br.com.joaogosmani.jgbiblioteca.models.Obra;
 
 public class EmprestimoService {
+
+    private static final int DIAS_PARA_DEVOLUCAO = 3;
     
     public Emprestimo novo(Cliente cliente, List<Obra> obras) {
         var emprestimo = new Emprestimo();
 
+        var dataEmprestimo = LocalDate.now();
+        var dataDevolucao = dataEmprestimo.plusDays(DIAS_PARA_DEVOLUCAO);
+
         emprestimo.setCliente(cliente);
         emprestimo.setObras(obras);
-        emprestimo.setDataEmprestimo(LocalDate.now());
-        emprestimo.setDataDevolucao(LocalDate.now().plusDays(3));
+        emprestimo.setDataEmprestimo(dataEmprestimo);
+        emprestimo.setDataDevolucao(dataDevolucao);
 
         return emprestimo;
     }
